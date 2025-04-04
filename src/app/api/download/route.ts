ecc830813b3140904a4807c7b82b8a9f2963abc6
Rashed Iqbal <dev.rashediqbal@gmail.com>
2025-04-04 20:27:37 +0600

    init changes

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -1,9 +1,15 @@
 import { NextResponse } from "next/server";
 import { BIN_DIR } from "ytdlp-nodejs";
+import fs from "fs";
 
 export async function GET() {
-  return new NextResponse(JSON.stringify({ message: "Hello World", BIN_DIR }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+  const items = fs.readdirSync(BIN_DIR);
+  console.log("Items in BIN_DIR:", items);
+  return new NextResponse(
+    JSON.stringify({ message: "Hello World", BIN_DIR, items }),
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    }
+  );
 }
