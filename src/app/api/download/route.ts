2a2c1415112e99dd241a5b683cd7a6b3a4ae2ee5
Rashed Iqbal <dev.rashediqbal@gmail.com>
2025-04-04 20:32:43 +0600

    init changes

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 import { BIN_DIR } from "ytdlp-nodejs";
 import fs from "fs";
+import path from "path";
 
 export async function GET() {
-  const items = fs.readdirSync(BIN_DIR);
+  const items = fs.readdirSync(path.join(BIN_DIR, "..", ".."));
   console.log("Items in BIN_DIR:", items);
   return new NextResponse(
     JSON.stringify({ message: "Hello World", BIN_DIR, items }),
