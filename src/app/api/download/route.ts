8c16a11920ea09122bddb8b05e7b5c8e42c29fa4
Rashed Iqbal <dev.rashediqbal@gmail.com>
2025-04-05 08:28:40 +0600

    init changes

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -4,10 +4,16 @@ import fs from "fs";
 import path from "path";
 
 export async function GET() {
-  const items = fs.readdirSync(path.resolve(process.cwd(), "node_modules"));
-  console.log("Items in BIN_DIR:", items);
+  const node_modules_path = path.join(process.cwd(), "node_modules");
+  const items = fs.readdirSync(node_modules_path);
+  console.log("Items in BIN_DIR:", items, node_modules_path);
   return new NextResponse(
-    JSON.stringify({ message: "Hello World", BIN_DIR, items }),
+    JSON.stringify({
+      message: "Hello World",
+      BIN_DIR,
+      items,
+      node_modules_path,
+    }),
     {
       status: 200,
       headers: { "Content-Type": "application/json" },
